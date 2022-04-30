66bb9dacd3a4d723695e062d6c1d044fb7f03adc
tanianasrin1 <tasrineva169@gmail.com>
2022-04-30 22:25:41 +0600

    extra page fixed

---
+++ src/Pages/Home/ExtraPart1/ExtraPart1.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const ExtraPart1 = () => {
+    return (
+        <div className='container mt-4'>
+            <h3 className='text-center text-uppercase'>Service</h3>
+            <div className='row text-center'>
+                <div className='col-lg-4 col-12'>
+                    <h3>healthy food</h3>
+                    <p className='small text-muted'>Henry doesn’t think fancier necessarily equals better, so forget fancy appetizers. A couple tried-and-true necessities will impress .</p>
+                </div>
+                <div className='col-lg-4 col-12'>
+                    <h3>product item</h3>
+                    <p className='small text-muted'>Depend  advises that you should avoid repeat ingredients in a menu. Depending on the ingredient in so forget fancy appetizers questio.</p>
+                </div>
+                <div className='col-lg-4 col-12'>
+                    <h3>food  Test</h3>
+                    <p className='small text-muted'>Food is any substance consumed to provide nutritional support for an organism. Food is usually  should avoid of plant  fungal origin</p>
+                </div>
+            </div>
+  
+  
+
+        </div>
+    );
+};
+
+export default ExtraPart1;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import ExtraPart1 from '../ExtraPart1/ExtraPart1';
 
 const Home = () => {
     return (
         <>
             <Banner></Banner>
+            <ExtraPart1></ExtraPart1>
         </>
     );
 };
