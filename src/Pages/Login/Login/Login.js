487da23efd33e1c20a8c047cc4d0777228b787de
tanianasrin1 <tasrineva169@gmail.com>
2022-05-04 20:14:57 +0600

    login fixed

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -3,6 +3,7 @@ import { Button, Form } from "react-bootstrap";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useLocation, useNavigate } from "react-router-dom";
 import auth from "../../../firebase.init";
+import SocialLogin from "../SocialLogin/SocialLogin";
 import './Login.css'
 
 const Login = () => {
@@ -36,29 +37,24 @@ const Login = () => {
     }
     
   return (
-    <div className="container w-50 mx-auto">
+    <div className="container login-form w-50 mt-5 mx-auto">
       <h2 className="text-center text-primary mt-2">Please Login !!!</h2>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
           <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+        
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
           <Form.Control ref={passwordRef} type="password" placeholder="Password"  required/>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+      
         <Button className="login-button" variant="primary" type="submit">
           Login
         </Button>
       </Form>
       <p>New to website? <span className="text-danger" onClick={navigateRegister}>Please Register</span> </p>
+      <SocialLogin></SocialLogin>
     </div>
   );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -2,6 +2,7 @@
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 import './Register.css'
 
 const Register = () => {
@@ -33,19 +34,19 @@ const Register = () => {
     }
 
     return (
-      <div className='register-form mt-4'>
-      <h2 style={{textAlign: 'center'}} className="register-title">Please Register!!!</h2>
-      <form  onSubmit={handleRegister} >
+      <div className='register-form mt-5'>
+      <h2 style={{textAlign: 'center'}} className="text-primary">Please Register!!!</h2>
+      <form className='mt-4 register' onSubmit={handleRegister} >
           <input type="text" name="name" id="" placeholder='Your name' />
           
           <input type="email" name="email" id="" placeholder='Your email' required/>
           
           <input type="password" name="password" id=""  placeholder='Your password' required/>
-          <input type="submit" value="Register" />
+          <input  type="submit" value="Register" />
       </form>
      
       <p>Already have an account? <span className="text-danger" onClick={navigateLogin}>Please Login</span> </p>
-      
+      <SocialLogin></SocialLogin>
   </div>
     );
 };
