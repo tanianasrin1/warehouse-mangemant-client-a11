deee8f1443a0612b5d29543c9e8c196a8501361e
tanianasrin1 <tasrineva169@gmail.com>
2022-05-08 17:54:50 +0600

    readme file fixed

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -5,7 +5,7 @@ import image from '../../images/image/f (1).jpg'
 
 const NotFound = () => {
     return (
-        <div className='text-center mt-4'>
+        <div className='text-center Not-found mt-4'>
             <h1 className='Found-title'>Not Found !!!</h1>
             <img className='mt-3' src={image} alt="" />
         </div>
